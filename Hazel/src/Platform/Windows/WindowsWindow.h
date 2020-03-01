d8dd5d3c3910f71a4aba1caa8a817910e61d3950
David Liljefors <david.liljefors@futuregames.nu>
2020-03-01 20:58:08 +0100

    Implemented basic shader for OpenGL

--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "Hazel/Window.h"
-
+#include "Hazel/Renderer/GraphicsContext.h"
 #include <GLFW/glfw3.h>
+
 
 namespace Hazel {
 
@@ -28,6 +29,7 @@ namespace Hazel {
 		virtual void Shutdown();
 	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context = nullptr;
 
 		struct WindowData
 		{
